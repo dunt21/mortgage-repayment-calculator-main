9727503df5ad28130435361bccd965ab6a974276
dunt21 <suadmacaulay50@gmail.com>
2025-05-21 00:02:50 +0000

    feat(ui): enhance form validation and improve layout for better user experience

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const endSec = document.getElementById("end");
 const monthlyAmt = document.getElementById("monthly-pay").textContent;
 const yearlyAmt = document.getElementById("total-pay").textContent;
 const userForm = document.getElementById("user-form");
-const interestLabel = document.querySelector(".interest-label");
 
 function clearInput(input) {
   input.value = "";
@@ -26,6 +25,19 @@ clearBtn.addEventListener("click", () => {
   radios.forEach((radio) => {
     radio.checked = false;
   });
+
+  const section = document.querySelectorAll("section");
+  section.forEach((section) => {
+    const inputLabel = section?.querySelector(".input-label");
+    const span = section?.querySelector("span");
+    const interestLabel = section?.querySelector(".interest-label");
+    const errorMsg = section?.querySelector(".error-msg");
+
+    inputLabel?.classList.remove("label-error");
+    span?.classList.remove("span-error");
+    errorMsg?.classList.add("hidden");
+    interestLabel?.classList.add("mb-6", "lg:mb-10");
+  });
 });
 
 mortgageAmt.addEventListener("input", (e) => {
@@ -36,58 +48,46 @@ mortgageAmt.addEventListener("input", (e) => {
 
 userForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const inputArea = document.querySelectorAll("input");
-  const inputLabel = document.querySelectorAll(".input-label");
-  const span = document.querySelectorAll("span");
-  const errorMsg = document.querySelectorAll(".error-msg");
-  console.log(errorMsg);
-  const optDiv = document.querySelector(".options");
 
   let isFilled = true;
 
   inputArea.forEach((input) => {
-    input.value.trim() === "" ? (isFilled = false) : "";
+    const section = input.closest("section");
+    const inputLabel = section?.querySelector(".input-label");
+    const span = section?.querySelector("span");
+    const errorMsg = section?.querySelector(".error-msg");
+
+    if (input.value.trim() === "") {
+      isFilled = false;
+      inputLabel?.classList.add("label-error");
+      span?.classList.add("span-error");
+      errorMsg?.classList.remove("hidden");
+    } else {
+      inputLabel?.classList.remove("label-error");
+      span?.classList.remove("span-error");
+      errorMsg?.classList.add("hidden");
+    }
   });
 
-  if (!isFilled) {
-    inputLabel.forEach((label) => {
-      label.classList.add("label-error");
-    });
+  const rSection = document.querySelector(".options");
+  const errorMsg = rSection?.querySelector(".error-msg");
+  const radioSelcted = rSection?.querySelector("input[name = radio]:checked");
+  const interestLabel = rSection?.querySelector(".interest-label");
 
-    span.forEach((span) => {
-      span.classList.add("span-error");
-    });
-
-    errorMsg.forEach((error) => {
-      error.classList.remove("hidden");
-    });
-
-    // interestLabel.classList.contains("mb-6")
-    //   ? interestLabel.classList.remove("mb-6")
-    //   : interestLabel.classList.remove("mb-10");
-
-    // interestLabel.classList.add("mb-2");
-
-    if (optDiv.document.querySelector("p")) {
-      interestLabel.classList.remove("mb-6", "mb-10");
-    }
+  if (!radioSelcted) {
+    errorMsg?.classList.remove("hidden");
+    interestLabel?.classList.remove("mb-6", "lg:mb-10");
+    interestLabel?.classList.add("mb-3");
   } else {
-    inputLabel.forEach((label) => {
-      label.classList.remove("label-error");
-    });
-
-    span.forEach((span) => {
-      span.classList.remove("span-error");
-    });
-
-    errorMsg.forEach((error) => {
-      error.classList.add("hidden");
-    });
-
-    const amt = Number(mortgageAmt.value);
-    const term = mortgageTerm.value * 12;
-    const rate = mortgageRate.value / 12;
+    errorMsg?.classList.add("hidden");
+    interestLabel?.classList.add("mb-6", "lg:mb-10");
+    interestLabel?.classList.remove("mb-3");
   }
+  const amt = Number(mortgageAmt.value);
+  const term = mortgageTerm.value * 12;
+  const rate = mortgageRate.value / 12;
 });
 
 // const monthlyPay = (amt * rate) / 1 - Math.pow(1 + rate, -term);
